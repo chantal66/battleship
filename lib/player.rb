cc838b232c986d155339930c5daea3e8bc8183ae
Chantal Justamond <cjustamm66@me.com>
2017-04-04 16:16:55 -0600

    ship placement  plus integration test are working

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,24 @@
+require './lib/player_board'
+
 class Player
   
-  attr_reader :shots
+  attr_reader :shots, :board
 
   def initialize(shots = 0)
     @shots = shots
-    
+    @board = PlayerBoard.new
+  end
+
+  def ship_position(ship_size, position)
+    positions = position.split
+    positions.each do |location|
+      row = board.swapp_letter(location[0])
+      board.player_ships.board[row][location[1].to_i] = ship_size.to_s
+    end  
+    board.player_ships.board
   end
 end
+
+# player = Player.new
+
+# p player.board.add_player_hits_and_misses('A2', 'H')
--- test/player_test.rb
+++ test/player_test.rb
@@ -10,6 +10,59 @@ class PlayerTest < Minitest::Test
     assert_instance_of Player, player
   end
 
+  def test_can_place_a_ship_in_certain_position
+    player = Player.new
+    result = player.ship_position(2,'A1')
+
+    assert_equal [["==========="],
+                 [". ", "1 ", "2 ", "3 ", "4 "], 
+                 ["A", "2", " ", " ", " "], 
+                 ["B", " ", " ", " ", " "], 
+                 ["C", " ", " ", " ", " "], 
+                 ["D", " ", " ", " ", " "], 
+                 ["==========="]], result
+  end
+
+  def test_can_place_a_ship_in_many_positions
+    player = Player.new
+    result = player.ship_position(2,'B1 B2')
+
+    assert_equal [["==========="],
+                 [". ", "1 ", "2 ", "3 ", "4 "], 
+                 ["A", " ", " ", " ", " "], 
+                 ["B", "2", "2", " ", " "], 
+                 ["C", " ", " ", " ", " "], 
+                 ["D", " ", " ", " ", " "], 
+                 ["==========="]], result
+  end
+
+  def test_player_has_a_board_to_play_with
+    player = Player.new
+    result = player.board
+
+    assert [["==========="], 
+            [". ", "1 ", "2 ", "3 ", "4 "], 
+            ["A", " ", " ", " ", " "], 
+            ["B", " ", " ", " ", " "], 
+            ["C", " ", " ", " ", " "], 
+            ["D", " ", " ", " ", " "], 
+            ["==========="]], result
+  end
+
+  def test_player_has_a_board_to_track_hits
+    player = Player.new
+    result = player.board.add_player_hits_and_misses('A2', 'H')
+
+    assert_equal 'H', result
+  end
+
+  def test_player_has_a_board_to_track_misses
+    player = Player.new
+    result = player.board.add_player_hits_and_misses('A3', 'M')
+
+    assert_equal 'M', result
+  end
+
   def test_shots_default_to_zero
     player = Player.new
     result = player.shots
@@ -17,13 +70,7 @@ class PlayerTest < Minitest::Test
     assert_equal 0, result
   end
 
-  def test_can_track_a_two_unit_ship
-    player = Player.new
-
-    binding.pry
-  end
-
-  def test_can_track_a_three_unit_ship
+  def test_can_player_fire_a_shot
     
   end
 end
