55334479336541a25a4cb33b756b038eb3b1f1cc
Chantal Justamond <cjustamm66@me.com>
2017-04-05 23:41:53 -0600

    validate_posibilities is working and tests passing :)

--- lib/validate_posibilities.rb
+++ lib/validate_posibilities.rb
@@ -18,6 +18,14 @@ module ValidatePosibilities
       final_location = present_location[0][0] + ['1','2','3','4']
     end
   end
+
+  def check_third_position(existing_locations)
+    if existing_locations[0][0] == existing_locations[1][0]
+      horizontal_alignment(existing_locations)
+    else
+      vertical_alignment(existing_locations)
+    end
+  end
   
   def all_adjacent_locations(previous_location)
     up_letter = "#{previous_location[0].next}#{previous_location[1]}"
@@ -36,4 +44,53 @@ module ValidatePosibilities
       place[0].between?('A', 'D') && place[1].between?('1', '4')
     end
   end
+
+  def too_many_letters?(positions)
+    positions.any? { |place| place.length > 2}
+  end
+
+  def positions_include_duplicates?(positions)
+    true if positions.uniq.size < positions.size
+  end
+
+  def position_wraps?(positions)
+    if contains(positions, 'A') && contains(positions, 'D')
+      true
+    elsif contains(positions, '1') && contains(positions, '4')
+      true
+    else
+      false
+    end
+  end
+
+  def contains(positions, char)
+    positions.any? { |place| place[0] == char }
+  end
+
+  def position_taken?(positions, player)
+    !positions.all? do |place|
+      player.board.player_ships.valid_positions.include?(place)
+    end
+  end
+
+  def positions_not_adjacent?(locations)
+    outcome = false
+    if locations.size < 2
+      outcome
+    else
+      index = 0
+      while index < locations.size - 1
+        unless all_adjacent_locations(locations[index]).include?(locations[index + 1])
+          outcome = true
+        end
+        index += 1
+      end
+    end
+    outcome
+  end
+  
+  def already_guessed?(input, player)
+    !player.board.player_hits_and_misses.valid_positions.include?(input)
+  end
 end
+# require 'pry'; binding.pry
--- test/validate_possibilities_test.rb
+++ test/validate_possibilities_test.rb
@@ -1,2 +1,78 @@
+require './test/test_helper'
 require './lib/validate_posibilities'
 require './lib/player'
+require 'pry'
+class ValidatePosibilitiesTest < Minitest::Test
+  include ValidatePosibilities
+
+  def test_it_can_find_vertical_aligment_for_three_unit
+    result = vertical_alignment(['A1','B1'])
+
+    assert_equal 'C1', result
+  end
+
+  def test_it_can_find_horizontal_alignmet_for_three_unit
+    result = horizontal_alignment(['A1','A2'])
+
+    assert_equal 'A3', result
+  end
+
+  def test_it_can_find_and_return_all_adjacent_locations
+    result = all_adjacent_locations('A1')
+    possibilities = ['B1', 'A2', '@1', 'A0']
+    assert_equal possibilities, result
+  end
+
+  def test_it_knows_if_a_single_input_is_in_the_wrong_format_or_off_the_board
+    result = position_wrong_format_or_outside_range?(['A9'])
+    assert result
+    result = position_wrong_format_or_outside_range?(['1B'])
+    assert result
+    result = position_wrong_format_or_outside_range?(['A2'])
+    refute result
+  end
+
+  def test_it_knows_if_multiple_inputs_are_wrong_format_or_off_board
+    result = position_wrong_format_or_outside_range?(['A1', 'A2'])
+    refute result
+    result = position_wrong_format_or_outside_range?(['A1', 'B5'])
+    assert result
+  end
+
+  def test_it_can_detect_duplicates_in_an_input_of_locations
+    result = positions_include_duplicates?(['A1', 'A2', 'A3'])
+    refute result
+    result = positions_include_duplicates?(['A1','A2','A2'])
+    assert result
+  end
+
+  def test_it_can_tell_if_an_input_wraps_around_the_board
+    result = position_wraps?(['A1','D1'])
+    assert result
+    result = position_wraps?(['B2','C2'])
+    refute result
+  end
+
+  def test_it_can_tell_if_an_input_contains_a_specific_character
+    result = contains(['A1','A2'], 'A')
+    assert result
+    result = contains(['A1','A2'], '4')
+    refute result
+  end
+
+  def test_it_can_tell_if_inputs_are_diagnonal_or_non_adjacent
+    result = positions_not_adjacent?(['A2','C3'])
+    assert result
+    result = positions_not_adjacent?(['A2','A3'])
+    refute result
+  end
+
+  def test_it_can_tell_if_a_location_has_already_been_guessed
+    player = Player.new
+    result = already_guessed?('A1', player)
+    refute result
+    player.board.player_hits_and_misses.valid_positions.delete('A1')
+    result = already_guessed?('A1', player)
+    assert result
+  end
+end
